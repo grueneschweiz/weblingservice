3f7b1cdf0f3fbf656bf7c7826181fbe08a1861ae
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-11-12 15:49:09 +0100

    add exceptions

--- app/Repository/Group/GroupRepository.php
+++ app/Repository/Group/GroupRepository.php
@@ -11,7 +11,10 @@
 namespace App\Repository\Group;
 
 
+use App\Exceptions\GroupNotFoundException;
+use App\Exceptions\WeblingAPIException;
 use App\Repository\Repository;
+use Webling\API\ClientException;
 
 class GroupRepository extends Repository {
 	
@@ -22,13 +25,24 @@ class GroupRepository extends Repository {
 	 * @param bool $cached
 	 *
 	 * @return Group
+	 *
+	 * @throws GroupNotFoundException
+	 * @throws ClientException on connection error
+	 * @throws WeblingAPIException
+	 *
+	 * @see https://gruenesandbox.webling.ch/api#header-error-status-codes
 	 */
-	public function get(int $id, bool $cached = true) {
+	public function get(int $id, bool $cached = true): Group {
 		// todo: implement this
 	}
 	
 	/**
 	 * Update the groups cache.
+	 *
+	 * @throws ClientException on connection error
+	 * @throws WeblingAPIException
+	 *
+	 * @see https://gruenesandbox.webling.ch/api#header-error-status-codes
 	 */
 	public function updateCache() {
 		// todo: implement this
