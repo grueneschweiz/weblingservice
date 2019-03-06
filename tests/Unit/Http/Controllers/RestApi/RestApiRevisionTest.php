b64e475c02e155fb81d4b4f7e381a04cd72fe2f9
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-03-06 12:15:27 +0100

    provide simple solution for testing

--- tests/Unit/Http/Controllers/RestApi/RestApiRevisionTest.php
+++ tests/Unit/Http/Controllers/RestApi/RestApiRevisionTest.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers\RestApi;
 
 
 use Tests\TestCase;
+use Tests\Unit\Http\Controllers\RestApi\AuthHelper;
 
 class RestApiRevisionTest extends TestCase {
+	/**
+	 * @var AuthHelper
+	 */
+	private $auth;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->auth = new AuthHelper($this);
+	}
+
+	public function tearDown() {
+		$this->auth->deleteToken();
+
+		parent::tearDown();
+	}
 
 	public function test_getRevision() {
-		$auth = $this->post( '/oauth/token', [
-				'grant_type'    => 'client_credentials',
-				'client_id'     => '1',
-				'client_secret' => 'A5SLmXcXyzfpLmbGyX7LvB6pnbkVwTbECiFwtCpR',
-				'scope'         => '',
-			]
-		);
-
-		$token = json_decode( $auth->getContent() )->access_token;
-
-		$response = $this->json( 'GET', '/api/v1/revision', [], [ 'Authorization' => 'Bearer ' . $token ] );
+		$response = $this->json( 'GET', '/api/v1/revision', [], $this->auth->getAuthHeader() );
 		$response->assertStatus( 200 );
 		$this->assertGreaterThan( 0, json_decode( $response->getContent() ) );
 	}
