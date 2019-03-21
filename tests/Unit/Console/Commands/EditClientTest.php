5f05f4975ed727d975d61efb8e8fc70a4b888384
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-03-21 11:53:18 +0100

    test commands

---
+++ tests/Unit/Console/Commands/EditClientTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyrill.bolliger
+ * Date: 2019-03-21
+ * Time: 11:23
+ */
+
+namespace App\Console\Commands;
+
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class EditClientTest extends TestCase {
+	private $clientId;
+
+	public function setUp() {
+		parent::setUp();
+
+		Artisan::call( 'client:add', [
+			'name'         => 'Unit Test',
+			'--root-group' => [ 10 ]
+		] );
+
+		$output = Artisan::output();
+		preg_match( "/Client ID: (\d+)/", $output, $clientId );
+
+		$this->clientId = (int) $clientId[1];
+	}
+
+	public function tearDown() {
+		Artisan::call( 'client:delete', [
+			'id' => [ $this->clientId ]
+		] );
+
+		parent::tearDown();
+	}
+
+	public function testHandle_successfulName() {
+		$this->artisan( 'client:edit', [
+			'id'     => $this->clientId,
+			'--name' => 'Edit Unit Test'
+		] )->expectsOutput( 'Successfully changed name.' )
+		     ->assertExitCode( 0 );
+	}
+
+	public function testHandle_successfulGroup() {
+		$this->artisan( 'client:edit', [
+			'id'           => $this->clientId,
+			'--root-group' => [ 20 ]
+		] )->expectsOutput( 'Deleted groups: 10' )
+		     ->expectsOutput( 'Added groups: 20' )
+		     ->assertExitCode( 0 );
+	}
+}
---
+++ tests/Unit/Console/Commands/ListClientTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyrill.bolliger
+ * Date: 2019-03-21
+ * Time: 11:39
+ */
+
+namespace App\Console\Commands;
+
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class ListClientTest extends TestCase {
+	private $clientId;
+
+	public function setUp() {
+		parent::setUp();
+
+		Artisan::call( 'client:add', [
+			'name'         => 'Unit Test',
+			'--root-group' => [ 10 ]
+		] );
+
+		$output = Artisan::output();
+		preg_match( "/Client ID: (\d+)/", $output, $clientId );
+
+		$this->clientId = (int) $clientId[1];
+	}
+
+	public function tearDown() {
+		Artisan::call( 'client:delete', [
+			'id' => [ $this->clientId ]
+		] );
+
+		parent::tearDown();
+	}
+
+	public function testHandle() {
+		$exitCode = Artisan::call( 'client:list' );
+		$output   = Artisan::output();
+
+		$this->assertEquals( 0, $exitCode );
+		$this->assertRegExp( '/^| ID\s+| Name\s+| Root Groups\s+| Created\s+|/', $output );
+		$this->assertRegExp( '/| ' . $this->clientId . '\s+| Unit Test\s+| 10\s+ | \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} |/', $output );
+	}
+}
