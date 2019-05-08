9198352df9dc0ed7f57e4515eee621823affd377
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-05-08 18:03:58 +0200

    add slack logging

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single','slack'],
         ],
 
         'single' => [
@@ -54,9 +54,9 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+            'username' => 'Webling Service',
+            'emoji' => ':skull_crossbones:',
+            'level' => 'notice',
         ],
 
         'stderr' => [
