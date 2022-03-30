6845db92043cff969a158f07df093bf1166c1a4c
Cyrill Bolliger <mail@cyrill.me>
2022-03-30 09:33:35 +0200

    [FIX] api key storage after upgrade
    Run: `php artisan migrate` to apply patch

---
+++ database/migrations/2022_03_30_072729_webling_allow_longer_encrypted_keys.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class WeblingAllowLongerEncryptedKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('webling_keys', function (Blueprint $table) {
+            $table->string('api_key', 256)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('webling_keys', function (Blueprint $table) {
+            $table->string('api_key', 255)->change();
+        });
+    }
+}
